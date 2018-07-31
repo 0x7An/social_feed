6a18309f0bbb416b3394e20b434f3a02805ae71a
Anderson <primataxdeus@gmail.com>
2018-07-31 15:56:30 -0300

    Installing body-parser and created the first route to add users

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const app = require('express')();
 const mongoose = require('mongoose');
 // This dependecy loads all files in one specific directory
 const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
 requireDir(dbConfig.modelsPath);
+
+app.use(bodyParser.json());
+
+app.post('/create', async (req, res) => {
+  const User = mongoose.model('User');
+
+  await User.create(req.body);
+
+  return res.send();
+});
 
 app.listen('3000');
