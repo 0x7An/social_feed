2604fa65a85218fd08107646c483669f04bc4610
Anderson <primataxdeus@gmail.com>
2018-08-02 15:50:44 -0300

    Authentication and JWT

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,8 @@ const requireDir = require('require-dir');
 const controllers = requireDir('./controllers');
 const routes = express.Router();
 
+// Auth
 routes.post('/signup', controllers.authController.signup);
+routes.post('/signin', controllers.authController.signin);
 
 module.exports = routes;
