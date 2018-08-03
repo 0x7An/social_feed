9e684cd5b0edadef610c10659302a6da6754cf87
Anderson <primataxdeus@gmail.com>
2018-08-03 17:22:13 -0300

    Configuring Sentry to better errors log

---
+++ config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  sentryDSN: 'https://72c93485358c4c4f8e18c5858790dcf3@sentry.io/1255790',
+};
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
 
+const Raven = require('./app/services/sentry');
+
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
@@ -11,7 +13,10 @@ requireDir(dbConfig.modelsPath);
 
 app.use(bodyParser.json());
 
+// app.use(Raven.requestHandler);
+
 app.use('/api', require('./app/routes'));
 
-app.listen('3000');
+app.use(Raven.errorHandler);
 
+app.listen('3000');
